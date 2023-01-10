3f9cd5c67fba1c2ce71840ddd83c93bdde604c8d
Guilherme Molari <yguilz123@gmail.com>
2023-01-10 17:39:27 +0000

    start routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
-  
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact  />
+      </Switch>
+    </Router>
   )
 }
 
