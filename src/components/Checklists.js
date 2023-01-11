87b552cdea5789fe7dfaf0eea8eab0bb63c59270
ghm <yguilz123@gmail.com>
2023-01-11 10:34:50 -0300

    page of checklist

--- src/components/Checklists.js
+++ src/components/Checklists.js
@@ -4,19 +4,43 @@ const checklists = {
         checks: {
             fiber: {
                 name: ['Fibra','fiber'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                    'Telefone utilizado para contato (Origem no DRA):',
+                    'Confirmado Endereço e telefone do cliente? Qual(is) número(s) foi(ram) confirmado(s)?',
+                    'Telefone com WhatsApp para contato?',
+                    'Ponto de referência? (No caso de agendamento)',
+                    'Cliente utiliza Roteador? Adquirido na Persis?',
+                    'Qual o login e senha de acesso ao roteador?',
+                    'Conexão com Roteador está OK?',
+                    'Sinal da fibra esta OK ? (mínimo 19, máxima 27)',
+                    'Lentidão/quedas ocorre em algum equipamento em particular? Caso sim, qual?',
+                    'Algum LED na ONU em vermelho ou laranja?',
+                    'PON piscando?',
+                    'Foi necessário realizar teste com cabo direto e Roteador desligado? A conexão com o cabo direto está OK?',
+                    'Apresentado algum Erro? Qual o número do Erro?',
+                    'Qual o procedimento realizado no atendimento? (detalhado)',
+                    'Qual o melhor período para um agendamento de O.S.?',
+                    'Foi alterado o tipo do atendimento?'
+                ]
             },
             building: {
                 name: ['Prédio/Condomínio', 'building'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
             radio: {
                 name: ['Radio', 'radio'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
             smart: {
                 name: ['Smartzap', 'smart'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
         }
     },
@@ -25,15 +49,21 @@ const checklists = {
         checks: {
             huawei: {
                 name: ['Huawei','huawei'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
             zte: {
                 name: ['ZTE','zte'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
             fiberhome_cdata: {
                 name: ['Fiberhome/C-DATA', 'fiberhome_cdata'],
-                q1: 'Responsável pela realização dos procedimentos (Cliente):'
+                questions: [
+                    'Responsável pela realização dos procedimentos (Cliente):',
+                ]
             },
         }
     },
--- src/pages/Checklist.jsx
+++ src/pages/Checklist.jsx
@@ -1,16 +1,28 @@
 import styles from './Checklist.module.css'
-import { useState } from 'react'
+import { Children, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import checklists from '../components/Checklists'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 const Checklist = () => {
     const {type, check} = useParams();
+    const [questions, setQuestions] = useState(checklists[type].checks[check].questions);
 
-    console.log(checklists[type].checks[check].name[0])
+    const h1 = <h1> Clipboard Copy </h1>
 
     return (
-        <div>
-            {checklists[type].checks[check].name[0]}
+        // <div>
+        //     {questions.map( question => 
+        //         <p> {question} </p>
+        //      )}
+        // </div>
+        <div className="App">
+            {h1}
+            <CopyToClipboard
+            text={h1.props.children}
+            onCopy={() => alert("Copied")}>
+            <button>Copy with this button</button>
+            </CopyToClipboard>
         </div>
     )
 
