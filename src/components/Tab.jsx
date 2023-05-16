f78011e2f81654def54795d1f486e0f57fd68e7e
Guilherme Molari <yguilz123@gmail.com>
2023-05-16 19:24:56 +0000

    delete tab change de index

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,14 +2,14 @@ import { useContext, useState, useEffect } from 'react'
 import styles from '../pages/Home.module.css'
 import './tab.css'
 import { Context } from '../context/Context'
-import { useCookies } from 'react-cookie'
 import svgCross from '../public/assets/x.svg'
 
 export default function Tab({indexx, check, type}){
     const {setIndex, setCheck, setType,tabs, attLocalTabs, index, deleteCookie} = useContext(Context)
-    const [cookies, setCookies, removeCookie] = useCookies()
     const [active, setActive] = useState(false);
     let del = false
+    let myPlace = tabs.indexOf(tabs.filter(i => indexx == i.index)[0])
+
 
     useEffect(() => {
         if (indexx == index) setActive(true)
@@ -19,9 +19,11 @@ export default function Tab({indexx, check, type}){
     function setInfos(e){
         setCheck(check)
         setIndex(indexx)
-        setType(type) 
+        setType(type)
         if (del) {
             deleteCookie(indexx)
+            setIndex(tabs[myPlace]?.index ? tabs[myPlace]?.index : tabs[myPlace-1]?.index ? tabs[myPlace-1]?.index : '')
+            setCheck(tabs[myPlace]?.check ? tabs[myPlace]?.check : tabs[myPlace-1]?.check ? tabs[myPlace-1]?.check : '')
             localStorage.removeItem(indexx)
         }  
         del = false
